253bcffae5c3902f7766d4a9d61c7142d877fb0a
ciarag01 <ciara.gribben@phs.scot>
2022-03-08 12:05:23 +0000

    Push new update of training and acoompanying example/exercise code

--- openxlsx_examples.R
+++ openxlsx_examples.R
@@ -1,274 +1,349 @@
-### 1 -  Setup ----
-# Packages
+###############################################################################
+# Openxlsx PHS Training
+# Example code to create workbook using Iris Dataset
+###############################################################################
+
+### 1 - Housekeeping ----
+# Load packages
+library(learnr)
 library(dplyr)
 library(janitor)
 library(stringr)
 library(openxlsx)
 library(ggplot2)
 
-# Read in data
-iris <- iris
-head (iris)
+### 2 - Creating a workbook -----
+wb <- createWorkbook()
 
-# Clean variable names and data
+# Adding worksheets
+# Add Notes Page
+addWorksheet(
+  # Name of the workbook we want to add the sheet to
+  wb = wb,
+  
+  # Define the name of the worksheet 
+  sheetName = "Notes",
+  
+  # Do we want to show gridlines? The default is TRUE so we have to specify if we don't want them
+  gridLines = FALSE,
+  
+  # Add headers - as a character vector for positions left, centre and right
+  # You can use some in built things
+  # (such as page numbers, date, time, filename etc)
+  header = c("&[Date]", NA, NA))
+
+# Add Data Page
+addWorksheet(
+  # Name of the workbook we want to add the sheet to
+  wb = wb,
+  
+  # Define the name of the worksheet 
+  sheetName = "Data",
+  
+  # Do we want to show gridlines? The default is TRUE so we have to specify if we don't want them
+  gridLines = FALSE,
+  
+  # Colour of the tab - either use from colours() list or use hex code (optional)
+  tabColour = "red", # Uses in built colour
+  
+  # Add headers - as a character vector for positions left, centre and right
+  # You can use some in built things
+  # (such as page numbers, date, time, filename etc)
+  header = c("ODD HEAD LEFT", "ODD HEAD CENTER", "ODD HEAD RIGHT"))
+
+# Add Charts Page
+addWorksheet(
+  # Name of the workbook we want to add the sheet to
+  wb = wb,
+  
+  # Define the name of the worksheet 
+  sheetName = "Charts",
+  
+  # Do we want to show gridlines? The default is TRUE so we have to specify if we don't want them
+  gridLines = FALSE,
+  
+  # Colour of the tab - either use from colours() list or use hex code (optional)
+  tabColour = "blue", # Uses in built colour
+  
+  # Add headers - as a character vector for positions left, centre and right
+  # You can use some in built things
+  # (such as page numbers, date, time, filename etc)
+  header = c("ODD HEAD LEFT", "ODD HEAD CENTER", "ODD HEAD RIGHT"))
+
+
+### 3 - Adding Tables -----
+# Read in iris data and clean
 data <- iris %>%
-clean_names() %>%
-mutate(species = str_to_title(species))
+  clean_names() %>%
+  mutate(species = str_to_title(species))
 
 # Create a table with the minimum and maximum petal length for each iris species
-data_table <- data %>%
-group_by(species) %>%
-summarise(max_pet_length = max(petal_length),
-max_pet_width = max(petal_width)) %>% 
-rename(Species = species, 
-`Max Petal Length` = max_pet_length,
-`Max Petal Width` = max_pet_width)
+min_max_data <- data %>%
+  group_by(species) %>%
+  summarise(max_pet_length = max(petal_length),
+            max_pet_width = max(petal_width)) %>% 
+  rename(Species = species, 
+         `Max Petal Length` = max_pet_length,
+         `Max Petal Width` = max_pet_width)
 
 # Create a table with a count of each iris species
-data_table2 <- data %>%
-group_by(species) %>%
-summarise(number = n())
+count_data <- data %>%
+  group_by(species) %>%
+  summarise(number = n())
 
-# Create a simple chart to include in the excel document
+# Write out min/max dataset as a static table
+writeData(
+  # Name of the workbook we want to add the sheet to
+  wb = wb,
+  
+  # Name of tab to save the data to 
+  sheet = "Data", 
+  
+  # Name of the object we want to write out
+  min_max_data, 
+  
+  # Row and column we want to have the top left cell of the data in
+  startCol = 2, 
+  startRow = 4)
+
+# Write out count dataset as a pivot table
+writeDataTable(
+  # Name of the workbook we want to add the sheet to
+  wb,
+
+  # Name of tab to save the data to
+  "Data",
+
+  # Name of the object we want to write out
+  count_data,
+
+  # Row and column we want to have the top left cell of the data in
+  startCol = 2,
+  startRow = 12)
+
+
+### 4 - Adding Charts -----
+# Create a chart to insert into the workbook and view
 graph1 <- data %>%
-ggplot(aes(sepal_length, sepal_width, color = species))+
-geom_point()+
-theme_classic()+
-labs(x="Sepal Length", y="Sepal Width",
-title = "Sepal Length by Sepal Width Across Different Species",
-color = "Species")
+  ggplot(aes(sepal_length, sepal_width, color = species))+
+  geom_point()+
+  theme_classic()+
+  labs(x="Sepal Length", y="Sepal Width",
+       color = "Species")
 
 graph1
 
+# Print the chart to ensure the correct one is used
+print(graph1)
 
-### 2 - Create initial excel output ----
-# Create workbook
-wb <- createWorkbook()
+# Insert chart into Charts tab of workbook
+insertPlot(wb = wb, 
+           
+           # Sheet number/name
+           sheet = "Charts",
+           
+           # Width/height of the graph - you might need to play around to get the right ratio
+           width = 6, 
+           height = 4, 
+           
+           # Define where the top and left side of the chart should sit
+           startRow = 3, 
+           startCol = 2, 
+           
+           # Resolution of the chart
+           dpi = 300) 
 
-# Add worksheet
-addWorksheet(
-  # Name of the workbook we want to add the sheet to
-  wb = wb,
-  
-  # Define the name of the worksheet 
-  sheetName = "Table1",
-  
-  # Show gridlines? Default is TRUE
-  gridLines = FALSE,
-  
-  # Colour of the tab - either use from colours() list or use hex code (optional)
-  tabColour = "red", # Uses in built colour
-  
-  # Add headers - as a character vector for positions left, centre and right
-  # You can use some in built things
-  # (such as page numbers, date, time, filename etc)
-  header = c("ODD HEAD LEFT", "ODD HEAD CENTER", "ODD HEAD RIGHT"))
+### 5 - Adding Formulae ----
+# Create a `Total` row at the bottom of the existing tab=le
+writeData(
+  wb,
+  "Data",
+  x = "Total",
+  startCol = 2,
+  startRow = 16)
 
-# Write data
-writeData(wb, 1, data_table, startCol = 2, startRow = 4)
-
-# Save
-saveWorkbook(
-# Workbook to save
-wb = wb,
-
-# The file path to save out to
-# - only short version here as working directory is set by the project
-file = "openxlsx_training.xlsx",
-
-# Whether you want to overwrite the file already there
-overwrite = TRUE)
+# Add the excel formula to calculate the total
+writeFormula(
+  wb,
+  "Data",
+  x = "=SUM(C13:C15)",
+  startCol = 3,
+  startRow = 16)
 
 
-### 3 - Creating Styles ----
-# Styles for the table headers, borders, the contents of any tables and any non-header text
-header_style <- createStyle(fontSize = 14,
-fontName = "Arial",
-halign = "Center", # Horizontal Align
-valign = "Center", # Vertical align
-wrapText = TRUE,
-# Can add multiple decorations in a vector
-textDecoration = c("bold", "underline"))
+### 6 - Styles -----
+# Create styles for the table headers, borders, the contents/totals of any tables 
+# and any non-header text
+header_style <- createStyle(fontSize = 11,
+                            fontName = "Arial",
+                            halign = "Left", # Horizontal Align
+                            valign = "Center", # Vertical align
+                            # Can add multiple decorations in a vector
+                            textDecoration = c("bold", "underline"))
 
 table_style <- createStyle(fontSize = 11,
-fontName = "Arial",
-valign = "Bottom",
-halign = "Right",
-border = "TopBottomLeftRight",
-numFmt = "COMMA")
+                           fontName = "Arial",
+                           valign = "Bottom",
+                           halign = "Right",
+                           border = "TopBottomLeftRight",
+                           numFmt = "COMMA")
 
-body_style <- createStyle(fontSize = 11,
-fontName = "Arial",
-valign = "Bottom",
-halign = "Left",
-border = "TopBottomLeftRight",
-numFmt = "TEXT")
+total_style <- createStyle(fontSize = 11,
+                          fontName = "Arial",
+                          valign = "Bottom",
+                          halign = "Center",
+                          border = "TopBottomLeftRight",
+                          numFmt = "TEXT",
+                          textDecoration = "bold")
 
 border_style_dash = createStyle(border = "TopBottomLeftRight",
-borderStyle = c("dashed"))
+                                borderStyle = c("dashed"))
 
 border_style_thick = createStyle(border = "TopBottomLeftRight",
-borderStyle = c("thick"))
+                                 borderStyle = c("thick"))
 
-# You can also define colours using hex codes and use these colours within styles:
-
+# Define blue and grey colours using hex codes 
 blue <- "#add8e6"
 gray <- "#ededed"
 
+# Use the defined colours to create new styles which colour cells
 blue_style = createStyle(
-fgFill = blue
+  fgFill = blue
 )
 
 gray_style = createStyle(
-fgFill = gray
+  fgFill = gray
 )
 
-### Applying styles
-wb <- createWorkbook()
-
-addWorksheet(
-  # Name of the workbook we want to add the sheet to
-  wb = wb,
-  
-  # Define the name of the worksheet 
-  sheetName = "Table1",
-  
-  # Show gridlines? Default is TRUE
-  gridLines = FALSE,
-  
-  # Colour of the tab - either use from colours() list or use hex code (optional)
-  tabColour = "red", # Uses in built colour
-  
-  # Add headers - as a character vector for positions left, centre and right
-  # You can use some in built things
-  # (such as page numbers, date, time, filename etc)
-  header = c("ODD HEAD LEFT", "ODD HEAD CENTER", "ODD HEAD RIGHT"))
-
-writeData(
-  
-  # Name of workbook to add the data to
-  wb, 
-  
-  # Sheet to insert data into
-  "Table1", 
-  
-  # Name of object we want to write
-  data_table, 
-  
-  # Which column/row should we start writing the data to
-  startCol = 2, startRow = 4)
-
-# Apply style to table body
+# Add total_style to the total row of table 2
 addStyle(
-  
-  # Name of workbook object we're working on
-  wb, 
-  
-  # Sheet to apply style to, can either use sheet number or sheet name
-  sheet = "Table1", 
-  
-  # Style to apply
-  style = body_style, 
-  
-  # Rows and Columns to apply style to
-  rows = 7, 
-  cols = 4)
 
-# Apply border style
+  # Name of workbook object we're working on
+  wb,
+
+  # Sheet to apply style to
+  "Data",
+
+  # Style to apply
+  style = total_style,
+
+  # Rows and Columns to apply style to
+  rows = 16,
+  cols = 2:3)
+
+# Add border styles to both tables
 addStyle(
   wb,
-  "Table1", 
+  "Data", 
   border_style_thick, 
-  rows = 4:7, cols = 2:4, gridExpand = TRUE,
+  rows = 4:7, 
+  cols = 2:4, 
+  gridExpand = TRUE,
   stack = TRUE)
 
-addStyle(wb, "Table1", blue_style, rows = 4:7, cols = 2:4, gridExpand = TRUE,
-         stack = TRUE)
+addStyle(
+  wb,
+  "Data", 
+  border_style_dash, 
+  rows = 12:16, 
+  cols = 2:3, 
+  gridExpand = TRUE,
+  stack = TRUE)
+
+# Apply colour to background of cells in Table 1        
+addStyle(
+  wb,
+  "Data", 
+  blue_style, 
+  rows = 4:7, 
+  cols = 2:4, 
+  gridExpand = TRUE,
+  stack = TRUE)
 
 
-saveWorkbook(
+### 7 - Formatting ----
+# Create information for the notes page 
+# Title
+contact <-  "Contact Details:"
+
+# Email
+email <- "mailto:first.last@phs.scot"
+names(email) <- "first.last@phs.scot"
+class(email) <- "hyperlink"
+
+# Description
+notes_text <- paste0("An overview of Fischer's iris data including 2 summary tables and a chart.")
+
+# Adding Information to notes page
+writeData(wb, sheet = "Notes", x = contact, startCol = 2, startRow = 9)
+writeData(wb, sheet = "Notes", x = email, startCol = 3, startRow = 9)
+writeData(wb,"Notes", notes_text, startCol = 2, startRow = 7)
+
+# Merge the cells containing the description text
+mergeCells(
+  # Specify workbook to apply to
+  wb, 
   
+  # Define worksheet for formatting to apply to
+  sheet = "Notes",
+  
+  # Define columns and rows to merge
+  cols = 2:7, rows = 7:8)
+
+# ncrease column width so that the 'Contact Details' text isn't cut off
+setColWidths(
+  # Specify workbook to apply to
+  wb, 
+  
+  # Define worksheet for formatting to apply to
+  sheet = "Notes", 
+  
+  # Columns to apply to
+  cols = 2, 
+  
+  # Width - check within excel by right clicking on column and clicking "Column Width"
+  widths = 15) 
+
+# Table Titles and column names
+## Table 1 - add table title and use header style to format it
+title_table1 <- "Max Petal Length and Width in Each Species Within Iris Dataset"
+writeData(wb, "Data", title_table1, startCol = 2, startRow = 2)
+addStyle(wb, "Data", header_style, rows = 2, cols = 2)
+
+## Table 1 - Column Names
+names(min_max_data) <- c("Species", "Max Petal Length", "Max Petal Width")
+writeData(wb, "Data", min_max_data, startCol = 2, startRow = 4)
+
+## Table 2 - add table title and use header style to format it
+title_table2 <- "Number of each Iris Species in Dataframe"
+writeData(wb, "Data", title_table2, startCol = 2, startRow = 10)
+addStyle(wb, "Data", header_style, rows = 10, cols = 2)
+
+## Table 2 - add better column names
+## Note that we cannot overwrite the cells containing existing pivot table 
+## So we will add a new pivot table underneath the existing one
+## Alternatively - rerun this script skipping lines 109 - 122 and the output
+# will have only one pivot table
+names(count_data) <- c("Species", "Number")
+writeDataTable(wb, "Data", count_data, startCol = 2, startRow = 18)
+
+## Chart - add chart title and use header style to format it
+chart_title <- "Chart 1: Sepal Length by Sepal Width Across Different Species"
+writeData(wb, "Charts", chart_title, startCol = 2, startRow = 2)
+addStyle(wb, "Charts", header_style, rows = 2, cols = 2)
+
+
+### 8 - Save Final workbook ----
+saveWorkbook(
   # Workbook to save
   wb = wb,
   
   # The file path to save out to
   # - only short version here as working directory is set by the project
-  file = "openxlsx_training.xlsx",
+  file = "iris_openxlsx_training.xlsx",
   
-  # Whether you want to overwrite the file already there
-  overwrite = TRUE)  
-
-#### 4 - Worksheet Formatting ----
-wb <- createWorkbook()
-
-addWorksheet(
-  wb = wb,
-  sheetName = "Data_Tables",
-  gridLines = FALSE,
-  tabColour = "red", # Uses in built colour
-  header = c("ODD HEAD LEFT", "ODD HEAD CENTER", "ODD HEAD RIGHT"))
-
-# Add a table title and better column names:
-title_table <- "Max Petal Length and Width in Each Species Within Iris Dataset"
-writeData(wb, "Data_Tables", title_table, startCol = 2, startRow = 2)
-
-names(data_table) <- c("Species", "Max Petal Length", "Max Petal Width")
-writeData(wb, "Data_Tables", data_table, startCol = 2, startRow = 4)
-
-
-### 5 - Excel Functionality -----
-## Pivot Table
-names(data_table2) = c("Species", "Number")
-
-# This function creates an actual table in excel
-writeDataTable(wb, sheet = "Data_Tables", startCol = 6, x = data_table2,
-               startRow = 4,
-               # You can check the names by hovering over table styles in excel
-               tableStyle = "TableStyleLight15",
-               # Names the table - not necessary in most situations though
-               tableName = "Data_Table_2")
-
-# Add a title for this table
-title_table2 <- "Number of each Iris Species in Dataframe"
-writeData(wb, "Data_Tables", title_table2, startCol = 6, startRow = 2)
-
-## Formulae
-# Adding "Total"
-writeData(wb, "Data_Tables",
-          # You can just add a string here, rather than saving to objects
-          # Better to save to an object if the string is long,
-          # for neatness and readability
-          x = "Total",
-          startCol = 6, startRow = 8)
-
-# Use this function to write excel formulae
-writeFormula(wb, "Data_Tables", x = "=SUM(G5:G7)",
-             startCol = 7, startRow = 8)
-
-
-### 6 - Adding Plots -----
-addWorksheet(
-  wb = wb,
-  sheetName = "Charts",
-  gridLines = FALSE,
-  tabColour = "blue", # Uses in built colour
-  header = c("ODD HEAD LEFT", "ODD HEAD CENTER", "ODD HEAD RIGHT"))
-
-print(graph1)
-
-insertPlot(wb = wb, 
-           sheet = 2,
-           width = 6, 
-           height = 4, 
-           startRow = 3, 
-           startCol = 2, 
-           dpi = 300) 
-
-saveWorkbook(
-  # Workbook object build in the beginning
-  wb = wb,
-  # The file path
-  # - only short version here as working directory is set by the project
-  file = "openxlsx_training.xlsx",
-  # Whether you want to overwrite the file already there
+  # If a file with the same name exists, do you want to overwrite it?
   overwrite = TRUE)
+
+
+##### END OF SCRIPT #####
---
+++ openxlsx_exercises.R
@@ -0,0 +1,18 @@
+###############################################################################
+# Openxlsx PHS Training
+# Exercises
+###############################################################################
+
+# Create your excel workbook using the Openxlsx package containing
+# (at least) the following:
+# * 3 worksheets
+# * 1 graph
+# * 1 table
+# * 2 colours using hexcodes (anywhere in workbook)
+# * 1 Email hyperlink
+# * Change row heights/column widths on one page
+
+# You can use any data you like but here's some ideas:
+# Data you use in your own team, 
+# Open data:  https://www.opendata.nhs.scot/,
+# TidyTuesday data: https://github.com/rfordatascience/tidytuesday
